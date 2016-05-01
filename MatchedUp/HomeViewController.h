60228594b6ea8fe08ea10ff1f00f9d06a13c87ad
aTo <aTo@ato_mac.home>
2016-05-01 11:30:18 +0200

    add homeViewController without contraints

---
+++ MatchedUp/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  MatchedUp
+//
+//  Created by aTo on 01/05/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
