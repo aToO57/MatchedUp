3ec571599431a8e5b3c9bddfe8253dbff9ba7add
aTo <aTo@ato_mac.home>
2016-05-01 12:04:00 +0200

    add ProfileViewController

---
+++ MatchedUp/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  MatchedUp
+//
+//  Created by aTo on 01/05/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
---
+++ MatchedUp/ProfileViewController.m
@@ -0,0 +1,45 @@
+//
+//  ProfileViewController.m
+//  MatchedUp
+//
+//  Created by aTo on 01/05/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *profilePictureImageView;
+@property (strong, nonatomic) IBOutlet UILabel *locationLabel;
+@property (strong, nonatomic) IBOutlet UILabel *ageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tagLineLabel;
+
+
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
