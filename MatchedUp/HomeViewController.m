6d4ad11692ed038e4c7be73a1ce584f9f4d020f1
aTo <aTo@ato_mac.home>
2016-05-01 12:36:56 +0200

    Setting up the HomeViewController

--- MatchedUp/HomeViewController.m
+++ MatchedUp/HomeViewController.m
@@ -9,23 +9,19 @@
 #import "HomeViewController.h"
 
 @interface HomeViewController ()
+
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *chatBarButtonItem;
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *settingsBarButtonItem;
-
 @property (strong, nonatomic) IBOutlet UIImageView *photoImageView;
-
 @property (strong, nonatomic) IBOutlet UILabel *firstNameLabel;
-
 @property (strong, nonatomic) IBOutlet UILabel *ageLabel;
-
 @property (strong, nonatomic) IBOutlet UILabel *tagLineLabel;
-
 @property (strong, nonatomic) IBOutlet UIButton *likeButton;
-
 @property (strong, nonatomic) IBOutlet UIButton *infoButton;
-
 @property (strong, nonatomic) IBOutlet UIButton *dislikeButton;
 
+@property (strong, nonatomic) NSArray *photos;
+@property (nonatomic) int currentPhotoIndex;
 
 @end
 
@@ -34,6 +30,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.likeButton.enabled = NO;
+    self.dislikeButton.enabled = NO;
+    self.infoButton.enabled = NO;
+    
+    self.currentPhotoIndex = 0;
+    
+    PFQuery *query = [PFQuery queryWithClassName:@"Photo"];
+    [query includeKey:@"user"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        if(!error){
+            self.photos = objects;
+        }
+        else{
+            NSLog(@"%@", error);
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
